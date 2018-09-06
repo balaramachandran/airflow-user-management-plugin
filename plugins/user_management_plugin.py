f0a97b7017ee8c2004142445e25d7e836183d24f
balaramachandran <14944307+balaramachandran@users.noreply.github.com>
2018-09-06 10:39:37 -0400

    Added some basic validations and security checks

--- plugins/user_management_plugin.py
+++ plugins/user_management_plugin.py
@@ -13,6 +13,7 @@ from flask_admin.contrib.sqla import ModelView
 from flask import Markup
 from flask import Blueprint, flash
 
+
 from wtforms import PasswordField
 
 import logging
@@ -26,13 +27,12 @@ Notes: User object has a superuser variable, but its not an actual column in the
 def email_formatter(v, c, m, p):
     return Markup('<a href="mailto:{m.email}">{m.email}</a>'.format(**locals()))
 
-
 class UserManagementModelView(ModelView):
 
     # http://flask-admin.readthedocs.io/en/latest/api/mod_model/
 
-    verbose_name_plural = "User Management - Password Users"
-    verbose_name = "User Management - Password User"
+    verbose_name_plural = "Change Passwords"
+    verbose_name = "Change Password"
     list_template = 'airflow/model_list.html'
     edit_template = 'airflow/model_edit.html'
     create_template = 'airflow/model_create.html'
@@ -41,61 +41,63 @@ class UserManagementModelView(ModelView):
     # page_size = 500
     can_set_page_size = True
 
-    can_create = False
+    can_create = True
     can_edit = True
-    can_delete = False
+    can_delete = True
 
     column_list = ('id', 'username', 'email')
     column_formatters = dict(email=email_formatter)
     column_searchable_list = ('username', 'email')
 
+
     form_columns = ('username', 'email', 'password')
-    # form_extra_fields = {
-    #     'password_confirm': PasswordField('Password (Confirm)')
-    # }
     form_widget_args = {
         'password': {
             'type': "password"
         }
     }
-    # form_create_rules = None
-    # form_args = dict(
-    #     email=dict(validators=[])
-    #)
 
+    # Added validation to make sure only the user itself or the Admin can change the details.
+    def on_form_prefill(self, form, id):
+        if (current_user.user.username != form.username.data and current_user.user.username != 'admin'):
+           flash('Trying to edit wrong user {u} while logged in as {cu}'.format(u=form.username.data, cu=current_user.user.username), 'error')
+           form.username.render_kw = {'readonly': True}
+           form.password.render_kw = {'readonly': True}
+           form.email.render_kw = {'readonly': True}
+
+    # Added the override for get_query so that the admin can see all the users while other's can see only their record displayed.
     def get_query(self):
-        username = ''
-        if hasattr(current_user, 'user'):
-           username = current_user.user.username
-        if username == 'admin':
+        if current_user.user.username == 'admin':
            return self.session.query(self.model)
 
         return  self.session.query(self.model).filter(self.model.username==username)
 
+    # Added the override for get_count_query so that the admin can see the count of all users while other's can see only one row displayed.
     def get_count_query(self):
-        username = ''
-        if hasattr(current_user, 'user'):
-           username = current_user.user.username
-        if username == 'admin':
+        if current_user.user.username == 'admin':
            return self.session.query(func.count('*')).filter(self.model.username!='')
 
         return  self.session.query(func.count('*')).filter(self.model.username==username)
 
-
     def on_model_change(self, form, model, is_created):
-        logging.info("UserManagementModelView.on_model_change(form=" + str(form) + ", model=" + str(model) + ", is_created=" + str(is_created) + ")")
-        return super(UserManagementModelView, self).on_model_change(form, model, is_created)
+
+        return super().on_model_change(form, model, is_created)
 
     # Overrides the create_model function to create the User object required to create the PasswordUser object
     def create_model(self, form):
         logging.info("UserManagementModelView.create_model(form=" + str(form) + ")")
-
+        # Only the Admin can create new user from this plugin.
+        if current_user.user.username != 'admin':
+           flash('Failed to create user. Only admin user can create new user', 'error')
+           logging.exception('Failed to create new user.')
+           return False
+           
         try:
             user = User() # Added this line to create the user object since its required to create the passworduser object
             model = self.model(user)
             form.populate_obj(model)
             self.session.add(model)
-            self._on_model_change(form, model, True)
+            self.on_model_change(form, model, True)
             self.session.commit()
         except Exception as ex:
             if not self.handle_view_exception(ex):
@@ -111,9 +113,8 @@ class UserManagementModelView(ModelView):
         return model
 
     def update_model(self, form, model):
+        # if model.username == self.cur_user :
         logging.info("UserManagementModelView.update_model(form=" + str(form) + ", model=" + str(model) + ")")
-        logging.info("form.password: " + str(form.password))
-        # logging.info("form.password_confirm: " + str(form.password_confirm))
         return super(UserManagementModelView, self).update_model(form, model)
 
     def delete_model(self, model):
@@ -122,7 +123,7 @@ class UserManagementModelView(ModelView):
 
 
 # Creating View to be used by Plugin
-user_management = UserManagementModelView(PasswordUser, Session, name="User Management", category="Admin", url="usermanagement")
+user_management = UserManagementModelView(PasswordUser, Session, name="Change Password", category="Admin", url="change_password")
 
 # Creating Blueprint
 user_management_bp = Blueprint(
@@ -143,3 +144,4 @@ class UserManagementPlugin(AirflowPlugin):
     executors = []
     admin_views = [user_management]
     menu_links = []
+    
